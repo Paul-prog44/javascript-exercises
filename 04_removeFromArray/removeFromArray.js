59e92227391510f8bbc19fb4dd06087d6b833b78
Paul-prog44 <saintouenfr@hotmail.com>
2023-10-12 18:18:17 +0200

    Finish exercice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) {
+    
+    for (const valueToBeRemoved of valuesToRemove) {
+        for (const value of array) {
+            if (valueToBeRemoved === value)
+            array.splice(array.indexOf(value), 1)
+        }
+    }
+    return array
+        
 };
 
 // Do not edit below this line
