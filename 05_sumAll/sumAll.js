80b25982edfb3d761e9b83862a6ea0e038b3a0d6
Paul-prog44 <saintouenfr@hotmail.com>
2023-10-12 18:46:44 +0200

    Finished ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(enterInt, exitInt) {
+let sum = 0
 
-};
+if  (typeof enterInt !== "number" || typeof exitInt !== "number" ) {
+    return "ERROR"
+} else {
+    if (enterInt < 0  || exitInt < 0) {
+        return "ERROR"
+    } else {
+        if (enterInt < exitInt) {
+            for (let i = enterInt; i <= exitInt; i++) {
+                sum += i
+            }
+            
+            } else if (enterInt > exitInt ) {
+                for (let i = exitInt; i <= enterInt; i++) {
+                    sum += i
+                }
+
+            }
+            return sum
+        };
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
